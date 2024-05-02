92a3b1012d4e6cc2962f6387e3db0ea12205720b
thony54 <thony.karter@gmail.com>
2024-05-02 13:46:30 -0500

    Nuevo Cambio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,29 @@
 'use strict';
 
-
 /**
  * navbar toggle in mobile
  */
 
-const /** {NodeElement} */ $navbar = document.querySelector("[data-navbar]");
-const /** {NodeElement} */ $navToggler = document.querySelector("[data-nav-toggler]");
+const $navbar = document.querySelector("[data-navbar]");
+const $navToggler = document.querySelector("[data-nav-toggler]");
 
 $navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
-
-
 
 /**
  * Header scroll state
  */
 
-const /** {NodeElement} */ $header = document.querySelector("[data-header]");
+const $header = document.querySelector("[data-header]");
 
 window.addEventListener("scroll", e => {
   $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
 
-
 /**
- Obtener referencia al elemento del gatito
+ * Obtener referencia al elemento del gatito
  */
 
- document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
   // Obtener referencia al elemento del gatito
   var gatito = document.querySelector('.gatito');
 
@@ -41,30 +37,21 @@ window.addEventListener("scroll", e => {
   }
 });
 
-<script>
-  // El color del enlace a azul al pasar el cursor por encima
-  function cambiarColorAzul() {
-    document.getElementById("quantum-code-link").style.color = "blue"
-  }
-
-  // El color del enlace a azul al pasar el cursor por encima
-  document.getElementById("quantum-code-link").addEventListener("mouseover", cambiarColorAzul);
-
-  // Función para redirigir a una página web al darle clic 
-  function irAPaginaWeb() {
-    window.location.href = "https://www.ejemplo.com"
-  }
-
-  // Evento para redirigir a una página web al darle clic
-  document.getElementById("quantum-code-link").addEventListener "click", function(event)
-    // Evitar el comportamiento predeterminado del enlace
-    event.preventDefault()
-    
-    // Redirigir a la página web después de 1 segundo (1000 milisegundos)
-    setTimeout(irAPaginaWeb, 1000)
-</script>
-
 /**
- * Add to favorite button toggle
+ * Cambiar color del enlace y redireccionar
  */
 
+function cambiarColorAzul() {
+  document.getElementById("quantum-code-link").style.color = "blue";
+}
+
+function irAPaginaWeb() {
+  window.location.href = "https://www.ejemplo.com";
+}
+
+document.getElementById("quantum-code-link").addEventListener("mouseover", cambiarColorAzul);
+
+document.getElementById("quantum-code-link").addEventListener("click", function(event) {
+  event.preventDefault();
+  setTimeout(irAPaginaWeb, 1000);
+});
